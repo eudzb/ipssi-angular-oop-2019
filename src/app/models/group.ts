38a2c56196ff91b7a0c2ab936b0889af6c51ef50
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2019-07-21 23:58:47 +0200

    clean up

--- src/app/models/group.ts
+++ src/app/models/group.ts
@@ -1,7 +1,6 @@
 import { Musicien } from './musicien';
 
 export interface Group {
-  id: number;
   name: string;
   musicien: Musicien[];
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Group } from '../../models/group';
+import { Musicien } from '../../models/musicien';
 
 @Component({
   selector: 'app-home',
@@ -7,31 +8,48 @@ import { Group } from '../../models/group';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-groups: Group[];
+groups: Group[] = [];
+musicien: Musicien[] = [];
+instruments = ['Drums', 'Piano', 'Guitar', 'Bass'];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    this.groups = [
-      {
-        id: 0,
-        name: 'super',
-        musicien: [
-          {
-            name: 'John',
-            instrument: 'Drums'
-          },
-          {
-            name: 'Eyden',
-            instrument: 'Guitar'
-          },
-          {
-            name: 'Blake',
-            instrument: 'Piano'
-          }
-        ]
-      }
-    ];
+    const numGroup = this.getRandomInt(1, 15);
+    for (let i = 0; i < numGroup; i++) {
+      this.groups.push({
+        name: this.randValue(6),
+        musicien: this.getRandomMusicien()
+      });
+    }
+  }
+
+  getRandomMusicien() {
+    const numMusicien = this.getRandomInt(1, 3);
+    for (let i = 0; i < numMusicien; i++) {
+      this.musicien.push({
+        name: this.randValue(6),
+        instrument: this.instruments[this.getRandomInt(0, 3)]
+      });
+    }
+    return this.musicien;
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  randValue(length) {
+    let result = '';
+    const characters = 'abcdefghijklmnopqrstuvwxyz';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
   }
 
 }
